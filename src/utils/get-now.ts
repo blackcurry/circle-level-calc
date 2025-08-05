8d94bf9a68b8373326d87e0d90cf69c2dcfc3261
blackcurry <whitecurry866@gmail.com>
2025-08-05 20:37:03 +0900

    Update get-now.ts

--- src/utils/get-now.ts
+++ src/utils/get-now.ts
@@ -1,5 +1,6 @@
 import dayjs from "dayjs";
 
 export const getNow = () => {
-  return dayjs().startOf("day").add(-5, "hour");
+  //return dayjs().startOf("day").add(-5, "hour");
+  return dayjs().add(-5, "hour");
 };
